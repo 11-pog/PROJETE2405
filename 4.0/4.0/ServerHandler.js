726e9c087f84e8eb26a63e75d7d7e02065eaf6ae
11-pog <alvessamuel872@gmail.com>
2024-09-11 09:14:58 -0300

    .

---
+++ 4.0/4.0/ServerHandler.js
@@ -0,0 +1,17 @@
+var http = require("http"),
+  fs = require("fs"),
+  path = require("path");
+
+const thisDir = __dirname;
+index = fs.readFileSync(path.dirname(thisDir) + "/index.html");
+
+const port = 45565;
+
+var app = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(index);
+});
+
+app.listen(port, () => {
+  console.log("Server connected to ${port}");
+});
